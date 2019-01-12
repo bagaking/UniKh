906fd540a87b1569bd8ba78657113d2c5cf2acf2
bagaking <zfaustk@qq.com>
2019-01-12 16:24:25 +0800

    refine (core): Motion.cs - create abstract base class

--- core/Motion.cs
+++ core/Motion.cs
@@ -2,30 +2,60 @@ using System;
 using UnityEngine;
 
 namespace UniKh.core {
-    public class Motion<T> {
 
+    public abstract class Motion {
+        
         public enum State {
             Ready,
             Running,
             Complete
         }
         
-        public delegate void MotionEvent(Motion<T> m);
+        public delegate void MotionEvent(Motion m);
 
         public event MotionEvent OnReady;
         public event MotionEvent OnStart;
         public event MotionEvent OnComplete;
         
+        public State state { get; private set; } = State.Ready;
+        
+        public float duration = 1;
+        protected float accumulateX = 0;
+        
+        public void Pulse(float deltaX) {
+            if (accumulateX < 0) return;
+            if (State.Complete == state) return;
+            accumulateX += deltaX;
+            
+            if (accumulateX >= duration) {
+                Update(1);
+                state = State.Complete;
+                OnComplete?.Invoke(this);
+            } else {
+                if (State.Running != state) {
+                    state = State.Running;
+                    OnStart?.Invoke(this);
+                }
+                Update(accumulateX / duration);
+            }
+        }
+
+        public Motion Reset() {
+            accumulateX = 0;
+            Update(0);
+            state = State.Ready;
+            OnReady?.Invoke(this);
+            return this;
+        }
+        
+        public abstract void Update(float rate);
+    }
+    
+    public class Motion<T> : Motion {
         public T posStart;
         public T posEnd;
-        public float duration = 1;
-
         public Func<T, T, float, T> Evaluate;
         public Action<T> SetValue;
-
-        public State state { get; private set; } = State.Ready;
-
-        private float accumulateX = 0;
 
         public Motion(T posStart, T posEnd, float duration, Func<T, T, float, T> Evaluate, Action<T> SetValue) {
             this.posStart = posStart;
@@ -35,32 +65,9 @@ namespace UniKh.core {
             this.SetValue = SetValue;
             Reset();
         }
-        
-        public void Pulse(float deltaX) {
-            if (accumulateX < 0) return;
-            if (State.Complete == state) return;
-            accumulateX += deltaX;
-            
-            if (accumulateX >= duration) {
-                SetValue(Evaluate(posStart, posEnd, 1));
-                state = State.Complete;
-                OnComplete?.Invoke(this);
-            } else {
-                if (State.Running != state) {
-                    state = State.Running;
-                    OnStart?.Invoke(this);
-                }
-                SetValue(Evaluate(posStart, posEnd, accumulateX / duration));
-            }
-            
-        }
 
-        public Motion<T> Reset() {
-            SetValue(posStart);
-            state = State.Ready;
-            accumulateX = 0;
-            OnReady?.Invoke(this);
-            return this;
+        public override void Update(float rate) {
+            SetValue(Evaluate(posStart, posEnd, rate));
         }
     }
 }
