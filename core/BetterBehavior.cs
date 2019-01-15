dd56289045d98322793c59cc104d5dade18fe73f
bagaking <zfaustk@qq.com>
2019-01-15 18:32:30 +0800

    feat (core): BetterBehavior.cs

---
+++ core/BetterBehavior.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace UniKh.core {
+    public abstract class BetterBehavior : MonoBehaviour {
+        private bool initiated = false;
+        private bool started = false;
+
+        private void OnEnable() {
+            if (!started) return;
+            WhenSetActive(true);
+        }
+
+        private void Start() {
+            if (!initiated) {
+                initiated = true;
+                Initial();
+            }
+            WhenSetActive(true);
+            started = true;
+        }
+
+        private void OnDisable() { WhenSetActive(false); }
+
+        protected abstract void Initial();
+
+        protected abstract void WhenSetActive(bool active);
+    }
+}
