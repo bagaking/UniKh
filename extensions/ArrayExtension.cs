0c17b2fc6f3ab67786fd9031b45a12f66dc264e9
bagaking <zfaustk@qq.com>
2019-09-17 19:48:06 +0800

    feat (extensions): ArrayExtension - add method RandomElem

--- extensions/ArrayExtension.cs
+++ extensions/ArrayExtension.cs
@@ -11,6 +11,12 @@ namespace UniKh.extensions {
             return ret;
         }
 
+        public static T RandomElem<T>(this T[] arr)
+        {
+            var rand = new Random();
+            return arr[rand.Next(arr.Length)];
+        }
+
         public static TResult Reduce<T, TResult>(this T[] lst, System.Func<TResult, T, TResult> reducer, TResult startVal) {
             var ret = startVal;
             for (var i = 0; i < lst.Length; i++) {
