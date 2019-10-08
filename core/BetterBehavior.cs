678b3643a59a35fdaf29c5c7457798e79cf8a7ab
bagaking <zfaustk@qq.com>
2019-10-08 15:19:55 +0800

    feat (core): refine betterBehavior

--- core/BetterBehavior.cs
+++ core/BetterBehavior.cs
@@ -1,28 +1,50 @@
+/** Copyright (C) 2019 Bagaking, All Rights Reserved */
+
 using UnityEngine;
 
 namespace UniKh.core {
     public abstract class BetterBehavior : MonoBehaviour {
-        private bool initiated = false;
-        private bool started = false;
+
+        private bool m_initiated = false;
+        private bool m_started = false;
+
+        /** events */
 
         private void OnEnable() {
-            if (!started) return;
-            WhenSetActive(true);
+            if (!m_started) return;
+            OnSetActive(true);
         }
 
         private void Start() {
-            if (!initiated) {
-                initiated = true;
-                Initial();
+            if (!m_initiated) {
+                m_initiated = true;
+                OnInit();
             }
-            WhenSetActive(true);
-            started = true;
+            OnSetActive(true);
+            m_started = true;
         }
 
-        private void OnDisable() { WhenSetActive(false); }
+        private void OnDisable() { OnSetActive(false); }
 
-        protected abstract void Initial();
+        /// <summary>
+        /// The initial method, put your init code here
+        /// </summary>
+        protected virtual void OnInit() { }
 
-        protected abstract void WhenSetActive(bool active);
+        /// <summary>
+        /// when the active state changes, the method will be call.
+        /// </summary>
+        protected virtual void OnSetActive(bool active) { }
+
+        /** helppers */
+
+        [System.NonSerialized]
+        private Transform m_myTransform;
+        protected Transform MyTransform {
+            get {
+                if (m_myTransform == null) m_myTransform = transform;
+                return m_myTransform;
+            }
+        }
     }
 }
