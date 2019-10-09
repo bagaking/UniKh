6e558c464f6e8c37f06b6ea4465b94cb35d038df
bagaking <zfaustk@qq.com>
2019-10-09 18:49:40 +0800

    example (csp): basic usage

---
+++ examples/ExampleCSP.cs
@@ -0,0 +1,53 @@
+/** TestCSP.cs
+ *  Author:         bagaking <kinghand@foxmail.com>
+ *  CreateTime:     2019/10/08 19:24:25
+ *  Copyright:      (C) 2019 - 2029 bagaking, All Rights Reserved
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniKh.core;
+using UniKh.extensions;
+using UniKh.core.csp;
+using UniKh.core.csp.waiting;
+
+public class ExampleCSP : BetterBehavior {
+
+    /// <summary>
+    /// The initial method of TestCSP.
+    /// This method **CAN** be removed
+    /// </summary>
+    protected override void OnSetActive(bool active) {
+        CSP.LazyInst.Do(Go());
+    }
+
+    protected IEnumerator Go() {
+        yield return Test1();
+        yield return Test2();
+    }
+
+    protected IEnumerator Test1() {
+        Time.timeScale = 0.5f;
+        var ind = 1;
+        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+        yield return 1; // wait for unity time - 1 sec
+        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+        yield return 1.2f; // wait for unity time - 1.2 sec
+        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+        yield return 1.5m; // wait for real time - 1.5 sec
+        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+        yield return null; // wait for unity frame - 1
+        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+    }
+
+    protected IEnumerator Test2() {
+        var timeNow = Time.time;
+        var ind = 1;
+        yield return new Condition(t => Time.time > timeNow + 1);
+        Debug.Log($"Test 2.{ind++}: {timeNow} => {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+    }
+
+
+}
