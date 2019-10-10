63857a92437d0b710f3939255a8bd8a5a304d9c7
bagaking <zfaustk@qq.com>
2019-10-10 14:13:36 +0800

    feat (example): CSP - add example scene

--- examples/ExampleCSP.cs
+++ examples/ExampleCSP.cs
@@ -13,53 +13,53 @@ using UniKh.extensions;
 using UniKh.core.csp;
 using UniKh.core.csp.waiting;
 
-public class ExampleCSP : BetterBehavior {
+namespace UniKh.example {
+    public class ExampleCSP : BetterBehavior {
 
-    /// <summary>
-    /// The initial method of TestCSP.
-    /// This method **CAN** be removed
-    /// </summary>
-    protected override void OnSetActive(bool active) {
-        CSP.LazyInst.Do(Go());
-    }
-
-    protected IEnumerator Go() {
-        yield return Test1();
-        yield return Test2();
-        yield return Test3();
-    }
-
-    protected IEnumerator Test1() {
-        Time.timeScale = 0.5f;
-        var ind = 1;
-        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
-        yield return 1; // wait for unity time - 1 sec
-        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
-        yield return 1.2f; // wait for unity time - 1.2 sec
-        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
-        yield return 1.5m; // wait for real time - 1.5 sec
-        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
-        yield return null; // wait for unity frame - 1
-        Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
-        Time.timeScale = 1;
-    }
-
-    protected IEnumerator Test2() {
-        var timeNow = Time.time;
-        var ind = 1;
-        yield return new Condition(t => Time.time > timeNow + 1);
-        Debug.Log($"Test 2.{ind++}: {timeNow} => {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
-    }
-
-    protected IEnumerator Test3() {
-        var timeNow = Time.time;
-        for (float i = 0, v = 1; i < 10000000; i ++) {
-            v /= 2;
-            v += i;
+        /// <summary>
+        /// The initial method of TestCSP.
+        /// This method **CAN** be removed
+        /// </summary>
+        protected override void OnSetActive(bool active) {
+            CSP.LazyInst.Do(Go());
         }
-        yield return 1;
-        Debug.Log($"Test 3: {timeNow} => {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+
+        protected IEnumerator Go() {
+            yield return Test1();
+            yield return Test2();
+            yield return Test3();
+        }
+
+        protected IEnumerator Test1() {
+            Time.timeScale = 0.5f;
+            var ind = 1;
+            Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+            yield return 1; // wait for unity time - 1 sec
+            Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+            yield return 1.2f; // wait for unity time - 1.2 sec
+            Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+            yield return 1.5m; // wait for real time - 1.5 sec
+            Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+            yield return null; // wait for unity frame - 1
+            Debug.Log($"Test 1.{ind++}: {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+            Time.timeScale = 1;
+        }
+
+        protected IEnumerator Test2() {
+            var timeNow = Time.time;
+            var ind = 1;
+            yield return new Condition(t => Time.time > timeNow + 1);
+            Debug.Log($"Test 2.{ind++}: {timeNow} => {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+        }
+
+        protected IEnumerator Test3() {
+            var timeNow = Time.time;
+            for (float i = 0, v = 1; i < 10000000; i++) {
+                v /= 2;
+                v += i;
+            }
+            yield return 1;
+            Debug.Log($"Test 3: {timeNow} => {Time.time} : {CSP.LazyInst.sw.ElapsedMilliseconds}");
+        }
     }
-
-
 }
