554474e976269e173365727e75c589ee5a97df81
bagaking <zfaustk@qq.com>
2019-10-23 14:11:34 +0800

    feat (editor): add EditorMenuItem

--- editor/EditorMenuItem.cs
+++ editor/EditorMenuItem.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UniKh.core;
+using UniKh.extensions;
+using UnityEditor;
 using UnityEngine;
 
 namespace UniKh.editor {
     public class EditorMenuItem {
-#if UNITY_EDITOR
+        
         [UnityEditor.MenuItem("UniKh/Create/Config List")]
         public static void CreateLst() {
             var so = ScriptableObject.CreateInstance<ConfigList>();
@@ -14,6 +17,35 @@ namespace UniKh.editor {
             UnityEditor.EditorUtility.FocusProjectWindow ();
             UnityEditor.Selection.activeObject = so;
         }
-#endif
+
+        private static string GetNewTransformName(Transform parent, string title = "c") {
+            if (parent == null) {
+                return title;
+            }
+            var children = parent.GetDirectChildren(n => n.name.StartsWith(title + ":"));
+            if (children.Count <= 0) return title + ":00";
+            var t = children[children.Count - 1];
+            return t.name.Replace(' ', '_').IncNumberTail(2);
+        }
+                
+        [MenuItem("GameObject/Kh UI Component/c <Empty>", false, 0)]
+        static void CreateUINodeEmpty(MenuCommand mc) {
+            var goParent = (mc.context as GameObject);
+            if (null == goParent) {
+                // todo
+                return;
+            }
+            var transParent = goParent.transform;
+            if (transParent == null) return;
+            
+            var go = new GameObject(GetNewTransformName(transParent, "c"));
+            go.transform.SetParent(transParent);
+            go.transform.localPosition = Vector3.zero;
+            go.transform.eulerAngles = Vector3.zero;
+            go.transform.localScale = Vector3.one;
+
+            Selection.activeObject = go;
+        }
+        
     }
 }
