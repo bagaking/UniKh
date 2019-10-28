83aa6a71b37de2afa63e0dba307432b732bb7340
bagaking <zfaustk@qq.com>
2019-10-29 02:51:34 +0800

    fix (components): fix panel animation

--- components/ui/molecules/panel/KhPanelGround.cs
+++ components/ui/molecules/panel/KhPanelGround.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 namespace UniKh.comp.ui {
     public class KhPanelGround : KhPanel {
-        protected override void OnInit() {
-            cg = this.GetOrAdd<CanvasGroup>();
-        }
-
         public CanvasGroup cg;
 
         public float startAlpha = 0;
-
+        
+        private float finalAlpha = 1f;
+        protected override void OnInit() {
+            cg = this.GetOrAdd<CanvasGroup>();
+            if (startAlpha == 1) return;
+            finalAlpha = cg.alpha;
+            cg.alpha *= startAlpha;
+        }
         protected override IEnumerator PlayAdmissionAnimation() {
             if (startAlpha == 1) yield break;
-            var finalAlpha = cg.alpha;
-            cg.alpha *= startAlpha;
             cg.DoFade(finalAlpha, durationShow).SetEase(easeActive);
             yield return null;
         }
--- components/ui/molecules/panel/KhPanelWindow.cs
+++ components/ui/molecules/panel/KhPanelWindow.cs
@@ -13,16 +13,20 @@ namespace UniKh.comp.ui {
         public Vector3 startOffset = Vector3.zero;
         public float startScaleRate = 1;
 
+        private Vector3 finalScale = Vector3.one;
+        protected override void OnInit() {
+            base.OnInit();
+            var trans = transform;
+            trans.localPosition += startOffset;
+            finalScale = trans.localScale;
+            trans.localScale *= startScaleRate;
+        }
+
         protected override IEnumerator PlayAdmissionAnimation() {
-            if (startOffset == Vector3.zero && startScaleRate ==1) {
+            if (startOffset == Vector3.zero && startScaleRate == 1) {
                 yield break;
             }
 //            var endPos = transform.localPosition;
-            var trans = transform;
-            var startPos = trans.localPosition + startOffset;
-            var finalScale = trans.localScale;
-            trans.localPosition = startPos;
-            trans.localScale *= startScaleRate;
             transform.TweenScale(finalScale, durationShow).SetEase(easeActive);
             transform.TweenMoveOffsetLocal(- startOffset, durationShow).SetEase(easeActive);
             yield return null;
