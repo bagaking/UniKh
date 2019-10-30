ae4a93a960fa147ae89308d520deab153848108e
bagaking <zfaustk@qq.com>
2019-10-30 19:05:27 +0800

    feat (extensions): array expand map method

--- extensions/ArrayExtension.cs
+++ extensions/ArrayExtension.cs
@@ -18,6 +18,12 @@ namespace UniKh.extensions {
             for (int i = 0; i < arr.Length; i++) ret[i] = converter(arr[i], i);
             return ret;
         }
+        
+        public static TResult[] Map<T, TResult>(this T[] arr, Func<T, int, T[], TResult> converter) {
+            var ret = new TResult[arr.Length];
+            for (int i = 0; i < arr.Length; i++) ret[i] = converter(arr[i], i, arr);
+            return ret;
+        }
 
         public static T RandomElem<T>(this T[] arr)
         {
