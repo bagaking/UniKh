4f859c5e0be0171ccf2389d33c7261cc4aec0b7e
bagaking <zfaustk@qq.com>
2019-11-14 20:18:13 +0800

    feat: remove unused Mgr

--- mgr/CorouMgr.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using UniKh.core;
-using UnityEngine;
-
-namespace UniKh.mgr {
-    public class CorouMgr : Singleton<CorouMgr> {
-        public static Coroutine Run(IEnumerator itr) { return Inst.StartCoroutine(itr); }
-    }
-}
