635854d9cbb6cce0dea1db8b3ff727cad2774fd2
bagaking <zfaustk@qq.com>
2020-04-14 10:09:43 +0800

    feat (ds): add LazyVal<T>

---
+++ dataStructure/LazyVal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UniKh.dataStructure {
+     
+    public class LazyVal<T> where T: class{
+        public T val;
+
+        private Func<T> _fnInitialVal = null;
+
+        public LazyVal(Func<T> fnInitialVal) {
+            _fnInitialVal = fnInitialVal;
+        }
+
+        public T Val {
+            get {
+                if (val == null) {
+                    val = _fnInitialVal();
+                }
+
+                return val;
+            }
+        }
+
+        public static implicit operator T(LazyVal<T> lazyLoader) {
+            return lazyLoader.Val;
+        }
+    }
+}
