2e774f47fb214f893a05b9c1fe861d2c1312824c
bagaking <zfaustk@qq.com>
2020-05-23 14:57:01 +0800

    feat (editor): add editor usage of MeshMerger, to merge mesh and generate prefab.

--- editor/menuItems/EditorMenuItem.cs
+++ editor/menuItems/EditorMenuItem.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Diagnostics;
+using Boo.Lang.Environments;
 using UniKh.core;
 using UniKh.extensions;
+using UniKh.utils;
 using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
 
 namespace UniKh.editor {
     public class EditorMenuItem {
@@ -25,6 +29,36 @@ namespace UniKh.editor {
             AssetDatabase.Refresh();
             EditorUtility.FocusProjectWindow();
             Selection.activeObject = so;
+        }
+
+        [MenuItem("UniKh/Utils/Merge Meshes Under Active Object")]
+        public static void MergeSelectedMeshes() {
+            if (!Selection.activeGameObject) {
+                Debug.Log("Please Select The Root Node.");
+            }
+
+            var mergedRenderer = MeshMerger.Batch(
+                Selection.activeGameObject.GetComponentsInChildren<MeshRenderer>(),
+                Selection.activeGameObject.transform
+            );
+            var assetName = "new-mesh@" + DateTime.Now.GetTimeStamp();
+            var prefabObj = new GameObject(assetName);
+            prefabObj.transform.position = Selection.activeGameObject.transform.position;
+            mergedRenderer.ForEach(
+                (mr, i) => {
+                    var mf = mr.GetComponent<MeshFilter>();
+                    var fullAssetName = assetName + "_" + i ;
+                    AssetDatabase.CreateAsset(mf.sharedMesh, "Assets/" + fullAssetName + ".asset");
+                    AssetDatabase.SaveAssets();
+                    var mesh = AssetDatabase.LoadAssetAtPath<Mesh>("Assets/" + fullAssetName + ".asset");
+                    mf.sharedMesh = mesh;
+                    mr.transform.parent = prefabObj.transform;
+                }
+            );
+            PrefabUtility.SaveAsPrefabAsset(prefabObj, "Assets/" + assetName + ".prefab");
+            var obj = PrefabUtility.InstantiatePrefab(PrefabUtility.LoadPrefabContents("Assets/" + assetName + ".prefab")) as GameObject;
+            obj.transform.position = prefabObj.transform.position;
+            Object.DestroyImmediate(prefabObj);
         }
     }
 }
