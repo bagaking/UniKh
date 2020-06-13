0a0e9dc227b71e255901f9d5c0eab185692a984c
bagaking <zfaustk@qq.com>
2020-06-13 16:38:45 +0800

    feat (components/../motion): Make MotionWindow.Show a promise

--- components/gadget/motion/MotionWindow.cs
+++ components/gadget/motion/MotionWindow.cs
@@ -4,8 +4,8 @@
  *  Copyright:      (C) 2019 - 2029 bagaking, All Rights Reserved
  */
 
-using System; 
-using UniKh.extensions; 
+using System;
+using UniKh.extensions;
 
 using System.Collections;
 using UniKh.core;
@@ -17,12 +17,12 @@ using UnityEngine;
 
 namespace UniKh.comp.ui {
     public class MotionWindow : MotionObject<MotionWindow> {
-        
+
         [Header("Motion Setting")]
         public Vector3 startOffset = Vector3.zero;
         public float startScaleRate = 1;
 
-        private Vector3 _initPosition = Vector3.zero; 
+        private Vector3 _initPosition = Vector3.zero;
         private Vector3 _initScale = Vector3.one;
 
         public bool phase;// { get; private set; } = false;
@@ -31,10 +31,10 @@ namespace UniKh.comp.ui {
 
         protected override void OnInit() {
             base.OnInit();
-            var trans = transform; 
-            _initPosition = trans.localPosition; 
-            _initScale = trans.localScale; 
-        } 
+            var trans = transform;
+            _initPosition = trans.localPosition;
+            _initScale = trans.localScale;
+        }
 
         protected override IEnumerator PlayAdmissionAnimation() {
             if (startOffset == Vector3.zero && startScaleRate == 1) {
@@ -43,7 +43,7 @@ namespace UniKh.comp.ui {
             Show();
             yield return null;
         }
-        
+
         protected override void OnSetActive(bool active) {
             var trans = transform;
             trans.localScale = _initScale * startScaleRate;
@@ -51,45 +51,49 @@ namespace UniKh.comp.ui {
             base.OnSetActive(active);
         }
 
-        private Tweener ExecuteAnimation(Tweener.Directions direction, Action<Tweener.State, Tweener.State> cb) {   
+        private Tweener ExecuteAnimation(Tweener.Directions direction, Action<Tweener.State, Tweener.State> cb) {
             transform.TweenScale(_initScale * startScaleRate, _initScale, durationShow).SetEase(easeActive).SetDirection(direction).MoveTo(0);
-            var tweener = transform.TweenMoveLocal(_initPosition + startOffset, _initPosition, durationShow).SetEase(easeActive).SetDirection(direction).MoveTo(0); 
+            var tweener = transform.TweenMoveLocal(_initPosition + startOffset, _initPosition, durationShow).SetEase(easeActive).SetDirection(direction).MoveTo(0);
             tweener.OnStateChanged += cb;
             return tweener;
         }
-         
+
         [Btn(true)]
-        public void Show() {
+        public Promise<object> Show() {
             if (startOffset == Vector3.zero && startScaleRate == 1) {
-                return;
+                return Skip.New.Start().AsPromise();
             }
 
             if (!gameObject.activeSelf) {
                 this.SetObjectActive();
-                return; // When this function 'Show' is called directly, OnInit has not been called yet.
+                return Skip.New.Start().AsPromise(); // When this function 'Show' is called directly, OnInit has not been called yet.
             }
-            
-            if (inAction) {
-                return;
-            }
-            inAction = true; 
 
-            ExecuteAnimation(Tweener.Directions.Forward, (from, to) => {
+            if (inAction) {
+                return Condition.New.Start(_ => !inAction).AsPromise();
+            }
+            inAction = true;
+
+            return ExecuteAnimation(Tweener.Directions.Forward, (from, to) => {
                 if (to <= Tweener.State.Active) return;
                 inAction = false;
-                phase = true; 
-            });
+                phase = true;
+            }).AsPromise().Then(_ => (object)_);
         }
-        
-        
+
+
         [Btn(true)]
+        private Promise<object> hide() {
+            return Hide(1);
+        }
+
         public Promise<object> Hide(float durationScale = 1) {
             if (startOffset == Vector3.zero && Math.Abs(startScaleRate - 1) < 0.0001f) {
                 return Skip.New.Start().AsPromise();
             }
-            
+
             if (inAction) {
-                return Condition.New.Start(_=>!inAction).AsPromise();
+                return Condition.New.Start(_ => !inAction).AsPromise();
             }
 
             inAction = true;
