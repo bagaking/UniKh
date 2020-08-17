ff8adb4fe9e45fd6e2bc320e80e870ffb64d963e
bagaking <zfaustk@qq.com>
2020-08-17 19:53:39 +0800

    feat (dataSource): reimplement Grid2D using Coord

--- dataSource/Grid2D.cs
+++ dataSource/Grid2D.cs
@@ -1,52 +1,41 @@
 using UniKh.dataStructure;
-using UniKh.extensions;
+using UniKh.space;
 using UniKh.utils;
 
 namespace UniKh.dataSource {
-    public class Grid2D {
-        public uint SizeVertical { get; }
-        public uint SizeHorizontal { get; }
+    public class Grid2D : Coord {
+        public V2I Center { get; }
 
-        public uint XCenter { get; }
-        public uint XMin { get; }
-        public uint XMax { get; }
-        public uint YCenter { get; }
-        public uint YMin { get; }
-        public uint YMax { get; }
+        private readonly int[] gridMarks;
 
-        private readonly int[][] gridMarks;
-
-        public int this[int y, int x] {
-            get => gridMarks[y][x];
-            set => gridMarks[y][x] = value;
+        public int this[int row, int col] {
+            get => gridMarks[Index(col, row)];
+            set => gridMarks[Index(col, row)] = value;
         }
         
-        public int this[V2I pos] {
-            get => gridMarks[pos.y][pos.x];
-            set => gridMarks[pos.y][pos.x] = value;
+        public int this[V2I coord] {
+            get => gridMarks[Index(coord)];
+            set => gridMarks[Index(coord)] = value;
+        }
+        
+        public int this[int index] {
+            get => gridMarks[index];
+            set => gridMarks[index] = value;
         }
 
-        public Grid2D(uint verticalSize, uint horizontalSize) {
-            SizeVertical = verticalSize;
-            SizeHorizontal = horizontalSize;
-            gridMarks = new int[SizeVertical][];
-            gridMarks.ForEach((_, i) => gridMarks[i] = new int[SizeVertical]);
-
-            XCenter = SizeHorizontal / 2;
-            XMin = 0;
-            XMax = SizeHorizontal - 1;
-            YCenter = SizeVertical / 2;
-            YMin = 0;
-            YMax = SizeVertical - 1;
+        public Grid2D(int rowSize, int colSize): base(rowSize, colSize) {
+            gridMarks = new int[Length];
+            Center = Size / 2;
         }
 
         public override string ToString() {
-            var sb = SGen.New["grid2D("][SizeVertical][','][SizeHorizontal][')'];
-            gridMarks.ForEach(
-                (row, i) => {
-                    sb.AppendLine().AppendFormat("{0:D2}", i).Append(". ");
-                    row.ForEach(v => sb.Append(v).Append(' '));
-                });
+            var sb = SGen.New["grid2D"][FullWindow];
+            for (var row = 0; row < Size.Row; row++) {
+                sb.AppendLine().AppendFormat("{0:D2}", row).Append(". ");
+                for (var col = 0; col < Size.Col; col++) {
+                    sb[this[col, row]].Append(' ');
+                }    
+            }
             return sb.End;
         }
     }
